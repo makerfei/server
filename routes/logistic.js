85f75008ad9f595f70a5b79a3d5b8e1899fec05d
zhangfei <zhangfei@sucang.cn>
2023-03-29 15:50:45 +0800

    fix:gnore

--- routes/logistic.js
+++ routes/logistic.js
@@ -1,21 +1,15 @@
 const router = require('koa-router')()
 const sql = require('../tool/sqlConfig')
 router.prefix('/api/logistic')
+const logisticConfig = require('../config/logistic')
+const moment = require('moment');
+router.all('/callback', async function (ctx, next) {
+    ctx.body = {
+        "EBusinessID": logisticConfig.EBusinessID,
+        "UpdateTime":moment(new Date()).format('YYYY-MM-DD HH:mm:ss'),
+        "Success": true,
+        "Reason": ""
+    }
 
-router.post('/upload/file', async function (ctx, next) {
-    let form = new formidable.IncomingForm()
-    let userId = ctx.session.userId;
-
-    ctx.body = await new Promise((res, inj) => {
-        form.parse(ctx.req,async function (err, fields, files) {
-            let fileType =   String(files.upfile.originalFilename).split('.')[1] || 'png';
-            let fileName = new Date().getTime();
-            let fullPath = `upload/${fileName}.${fileType}`
-            fs.copyFileSync(files.upfile.filepath, path.join(__dirname, `../public/upload`, `${fileName}.${fileType}`));
-
-            fs.writeSync()
-            res({ "code": 0, "data": { "url": fullPath }, "msg": "success" })
-        })
-    })
 })
 module.exports = router
