39e0aae54f2b71fe0690e2d24554267cb6753d56
zhangfei <zhangfei@sucang.cn>
2023-04-09 00:35:05 +0800

    fix:邮件监听

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,7 @@ const router = require('koa-router')()
 const sql = require('../../tool/sqlConfig')
 router.prefix('/api/admin')
 
+//获取用户当前信息
 router.get('/currentUser', async function (ctx, next) {
   let res = await sql.promiseCall({ sql: `select * from user where id =?`, values: [ctx.session.userId] });
 
@@ -63,296 +64,188 @@ router.get('/currentUser', async function (ctx, next) {
   }
 })
 
-router.get('/goodsList', async function (ctx, next) {
+//后端退出登录
+router.post('/outLogin', async function (ctx, next) {
+  ctx.session.userId = 0
+  ctx.session.userType = 0
   ctx.body = {
-    data: [
-      {
-        key: 99,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 99',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 503,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 81,
-      },
-      {
-        key: 98,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 98',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 164,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 12,
-      },
-      {
-        key: 97,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 97',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 174,
-        status: '1',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 81,
-      },
-      {
-        key: 96,
-        disabled: true,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 96',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 914,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 7,
-      },
-      {
-        key: 95,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 95',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 698,
-        status: '2',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 82,
-      },
-      {
-        key: 94,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 94',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 488,
-        status: '1',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 14,
-      },
-      {
-        key: 93,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 93',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 580,
-        status: '2',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 77,
-      },
-      {
-        key: 92,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 92',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 244,
-        status: '3',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 58,
-      },
-      {
-        key: 91,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 91',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 959,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 66,
-      },
-      {
-        key: 90,
-        disabled: true,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 90',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 958,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 72,
-      },
-      {
-        key: 89,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 89',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 301,
-        status: '2',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 2,
-      },
-      {
-        key: 88,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 88',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 277,
-        status: '1',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 12,
-      },
-      {
-        key: 87,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 87',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 810,
-        status: '1',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 82,
-      },
-      {
-        key: 86,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 86',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 780,
-        status: '3',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 22,
-      },
-      {
-        key: 85,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 85',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 705,
-        status: '3',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 12,
-      },
-      {
-        key: 84,
-        disabled: true,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 84',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 203,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 79,
-      },
-      {
-        key: 83,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 83',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 491,
-        status: '2',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 59,
-      },
-      {
-        key: 82,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 82',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 73,
-        status: '0',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 100,
-      },
-      {
-        key: 81,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
-        name: 'TradeCode 81',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 406,
-        status: '3',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 61,
-      },
-      {
-        key: 80,
-        disabled: false,
-        href: 'https://ant.design',
-        avatar: 'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
-        name: 'TradeCode 80',
-        owner: '曲丽丽',
-        desc: '这是一段描述',
-        callNo: 112,
-        status: '2',
-        updatedAt: '2022-12-06T05:00:57.040Z',
-        createdAt: '2022-12-06T05:00:57.040Z',
-        progress: 20,
-      },
-    ],
-    total: 100,
-    success: true,
-    pageSize: 20,
-    current: 1,
+    status: 'ok',
+    currentAuthority: 'admin',
+  }
+
+})
+
+
+//获取用户列表
+router.get('/userList', async function (ctx, next) {
+  let query = ctx.request.query
+  let { current = 1, pageSize = 10 } = query;
+  let listVal = [];
+  let listVarStr = ''
+  for (const key in query) {
+    if (key != 'current' && key != 'pageSize' && query[key] !== '') {
+      listVarStr += listVarStr === '' ? ` where ${key}=? ` : ` and ${key}=? `
+      listVal.push(query[key])
+    }
+  }
+  let orderInfoSql = await sql.promiseCall({
+    sql: `select *  from user   ${listVarStr}   order    by id desc limit ${(current - 1) * pageSize},${pageSize}  `,
+    values: listVal
+  })
+  ctx.body = { "code": 0, "data": orderInfoSql.results || [], "msg": "success" }
+})
+
+//商品类目管理
+router.post('/category/change', async function (ctx, next) {
+  let { id = '', isUse = '', name = '', paixu = '', pid = '' } = ctx.request.body;
+  if (id && name) {
+    await sql.promiseCall({
+      sql: `update category set isUse=?,name=?,paixu=?,pid=? where id=?`
+      , values: [isUse, name, paixu, pid, id]
+    })
+  } else if (name) {
+    await sql.promiseCall({
+      sql: ` INSERT INTO category (isUse, name, paixu, pid) VALUES(?,?,?,?)`
+      , values: [isUse, name, paixu, pid]
+    })
+  } else if (id) {
+    await sql.promiseCall({
+      sql: `delete  from category WHERE id=?`
+      , values: [id]
+    })
+  }
+  ctx.body = { code: 0, msg: 'success' }
+})
+
+//获取商品类目
+router.get('/category/all', async function (ctx, next) {
+  let categorySql = await sql.promiseCall({ sql: `select * from category   ORDER BY paixu ASC`, values: [] });
+  if (!categorySql.error) {
+    ctx.body = { "code": 0, "data": categorySql.results, "msg": "success" }
+  } else {
+    ctx.body = { code: -1, msg: categorySql.error.message }
   }
 })
+
+//获取商品列表
+router.get('/goods/list', async function (ctx, next) {
+  let query = ctx.request.query
+  let { current = 1, pageSize = 10 } = query;
+  let listVal = [];
+  let listVarStr = ''
+  for (const key in query) {
+    if (key != 'current' && key != 'pageSize' && query[key] !== '') {
+      listVarStr += listVarStr === '' ? ` where ${key}=? ` : ` and ${key}=? `
+      listVal.push(query[key])
+    }
+  }
+  let orderInfoSql = await sql.promiseCall({
+    sql: `select id,categoryId,pic,name,minPrice,recommendStatus,stores,afterSale,status  from goods   ${listVarStr}   order    by id desc limit ${(current - 1) * pageSize},${pageSize}  `,
+    values: listVal
+  })
+  ctx.body = { "code": 0, "data": orderInfoSql.results || [], "msg": "success" }
+})
+
+
+router.get('/goods/detail', async function (ctx, next) {
+  let query = ctx.request.query
+  let { id } = query;
+  let orderInfoSql = await sql.promiseCall({
+    sql: `select *  from goods    where id =? `,
+    values: [id]
+  })
+
+  let picsListSql = await sql.promiseCall({
+    sql: `select *  from pics    where goodsId =? `,
+    values: [id]
+  })
+
+
+  ctx.body = {
+    "code": 0, "data": {
+      picsList: (picsListSql.results || []).map(item => {
+        return {
+          // ...item,
+          uid: item.id,
+          name: item.type ? (item.pic + '  视频') : item.pic,
+          status: 'done',
+          url: item.type ? (item.pic + '_0.jpg') : item.pic,
+          saveType: item.type
+        }
+      }), ...orderInfoSql.results[0] || {}
+    }, "msg": "success"
+  }
+})
+
+router.post('/goods/picsAdd', async function (ctx, next) {
+  let { goodsId, type, url } = ctx.request.body;
+  let picsListSql = await sql.promiseCall({
+    sql: `INSERT INTO pics ( goodsId, pic, type) VALUES (?, ?, ?)`,
+    values: [goodsId, url, type]
+  })
+  ctx.body = { code: 0, data: picsListSql.results.insertId }
+})
+
+router.post('/goods/picsDel', async function (ctx, next) {
+  let { id } = ctx.request.body;
+  let picsListSql = await sql.promiseCall({
+    sql: `DELETE from pics where id = ?`,
+    values: [id]
+  })
+  ctx.body = { code: 0 }
+})
+
+
+
+
+
+
+
+
+//商品详情修改
+router.post('/goods/change', async function (ctx, next) {
+
+  let { id = '', afterSale = '', categoryId = '', characteristic = '', content = '', feeType = '',
+    isFree = '', logisticsId = '', minPrice = '', name = '', originalPrice = '',
+    pic = '', recommendStatus = '', status = '', stores = '' } = ctx.request.body;
+  if (id && name) {
+    await sql.promiseCall({
+      sql: `update goods set 
+      afterSale = ?, categoryId = ?, characteristic = ?, content = ?, feeType = ?,
+    isFree = ?, logisticsId = ?, minPrice = ?, name = ?, originalPrice = ?,
+    pic = ?, recommendStatus = ?, status = ?, stores = ?
+      
+      
+      where id=?`
+      , values: [afterSale, categoryId, characteristic, content, feeType,
+        isFree, logisticsId, minPrice, name, originalPrice,
+        pic, recommendStatus, status, stores, id]
+    })
+  } else if (name) {
+    await sql.promiseCall({
+      sql: ` INSERT INTO goods (afterSale, categoryId, characteristic, content, feeType,
+        isFree, logisticsId, minPrice, name, originalPrice,
+        pic, recommendStatus, status, stores) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
+      , values: [afterSale, categoryId, characteristic, content, feeType,
+        isFree, logisticsId, minPrice, name, originalPrice,
+        pic, recommendStatus, status, stores]
+    })
+  } else if (id) {
+    await sql.promiseCall({
+      sql: `delete  from goods WHERE id=?`
+      , values: [id]
+    })
+  }
+  ctx.body = { code: 0, msg: 'success' }
+})
+
+
+
+
+
+
+
+
 
 
 
--- routes/dfs.js
+++ routes/dfs.js
@@ -4,17 +4,44 @@ router.prefix('/api/dfs')
 var formidable = require('formidable');
 const path = require('path')
 const fs = require('fs')
+const sms = require('../config/sms')
+
+
+var COS = require('cos-nodejs-sdk-v5');
+var cos = new COS({
+    SecretId: sms.secretId,
+    SecretKey: sms.secretKey,
+});
+
+
 router.post('/upload/file', async function (ctx, next) {
     let form = new formidable.IncomingForm()
     let userId = ctx.session.userId;
 
     ctx.body = await new Promise((res, inj) => {
-        form.parse(ctx.req,async function (err, fields, files) {
-            let fileType =   String(files.upfile.originalFilename).split('.')[1] || 'png';
+        form.parse(ctx.req, async function (err, fields, files) {
+            let fileType = String(files.upfile.originalFilename).split('.')[1] || 'png';
             let fileName = new Date().getTime();
-            let fullPath = `upload/${fileName}.${fileType}`
-            fs.copyFileSync(files.upfile.filepath, path.join(__dirname, `../public/upload`, `${fileName}.${fileType}`));
-            res({ "code": 0, "data": { "url": fullPath }, "msg": "success" })
+            let fullPathCos = `img/${fileName}.${fileType}`
+           // fs.copyFileSync(files.upfile.filepath, path.join(__dirname, `../public/upload`, `${fileName}.${fileType}`));
+            const filePath = files.upfile.filepath // 本地文件路径
+          await  cos.putObject({
+                Bucket: 'h5-1316824427', /* 填入您自己的存储桶，必须字段 */
+                Region: 'ap-shanghai',  /* 存储桶所在地域，例如 ap-beijing，必须字段 */
+                Key: fullPathCos,  /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
+                StorageClass: 'STANDARD',
+                /* 当 Body 为 stream 类型时，ContentLength 必传，否则 onProgress 不能返回正确的进度信息 */
+                Body: fs.createReadStream(filePath), // 上传文件对象
+                ContentLength: fs.statSync(filePath).size,
+                onProgress: function (progressData) {
+                    console.log(JSON.stringify(progressData));
+                }
+            }, function (err, data) {
+                res({ "code": 0, "data": { "url":`https://www.mgdg.shop/${fullPathCos}`  }, "msg": "success" })
+            });
+
+          
+
         })
     })
 })
